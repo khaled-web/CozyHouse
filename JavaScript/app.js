0cad57c47f6e62f429c6dcee3fd2ed1a921dfc18
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2021-12-04 15:52:31 +0200

    Local Storage is physically completed

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -83,13 +83,21 @@ class UI {
 
 // local storage
 class Storage {
-
+ static saveProducts(products) {
+  localStorage.setItem('products', JSON.stringify(products))
+ }
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
  const ui = new UI();
  const products = new Products();
 
+
  // get all products
- products.getProducts().then(products => ui.displayProducts(products));
+ products.getProducts().then(products => {
+  ui.displayProducts(products)
+  Storage.saveProducts(products);
+ });
+
 });
